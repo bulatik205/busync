a5017dd43689a2f945bcf04ef13457625852816c
Булат Муллагалиев <bulatmullagal@gmail.com>
2026-01-29 08:09:18 +0400

    parse path and create base path file

---
+++ config/pathGet.php
@@ -0,0 +1,30 @@
+<?php
+function getBackPath($fromDir) {
+    $root = realpath($_SERVER['DOCUMENT_ROOT']);
+    $from = realpath($fromDir);
+    
+    $root = str_replace('\\', '/', $root);
+    $from = str_replace('\\', '/', $from);
+    
+    if (strpos($from, $root) !== 0) {
+        return '/'; 
+    }
+    
+    $relative = str_replace($root, '', $from);
+    
+    $relative = ltrim($relative, '/');
+    
+    if (empty($relative)) {
+        return './';
+    }
+    
+    $depth = substr_count($relative, '/');
+    
+    if ($depth == 0 && !empty($relative)) {
+        $depth = 1;
+    }
+    
+    return str_repeat('../', $depth);
+}
+
+define('BASE_PATH', getBackPath(__DIR__));
