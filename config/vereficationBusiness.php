7328bf8125c940de41fd67ef8768918c91baaffb
Булат Муллагалиев <bulatmullagal@gmail.com>
2026-01-31 14:16:18 +0400

    new: a file and a function in this file to check the existence of a business

---
+++ config/vereficationBusiness.php
@@ -0,0 +1,29 @@
+<?php
+function vereficationBusiness($pdo, $userId) : bool {
+    try {
+        if ($userId === null) {
+            return false;
+        }
+
+        if (!is_numeric($userId)) {
+            return false;
+        }
+
+        if (strlen($userId) > 72) {
+            return false;
+        }
+        
+        $stmt = $pdo->prepare("SELECT * FROM business_info WHERE user_id = ? LIMIT 1");
+        $stmt->execute([$userId]);
+        $businessInfo = $stmt->fetch(PDO::FETCH_ASSOC);
+        
+        if (!empty($businessInfo)) {
+            return true;
+        } 
+
+        return false;
+    } catch (Exception $e) {
+        error_log("Auth error: " . $e->getMessage());
+        return false;
+    }
+} 
