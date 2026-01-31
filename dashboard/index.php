1acac9b4f123917daf0c55bece3c1b28557ded0e
Булат Муллагалиев <bulatmullagal@gmail.com>
2026-01-31 20:12:37 +0400

    rename and update: added authorization and business registration handler

--- dashboard/index.php
+++ dashboard/index.php
@@ -1,3 +1,18 @@
+<?php 
+session_start();
+require_once '../config/config.php';
+define('BASE_PATH', getBackPath(__DIR__));
+
+if (verifyAuth($pdo) === false) {
+    header('Location: ' . BASE_PATH . 'login/');
+    exit;
+}
+
+if (verificationBusiness($pdo, $_SESSION['user_id']) === false) {
+    header('Location: ' . BASE_PATH . 'profile/reg/');
+    exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
