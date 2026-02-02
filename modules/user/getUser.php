74e2dc44e464a63c33d4a12b455d4a6723ce1dc1
Булат Муллагалиев <bulatmullagal@gmail.com>
2026-02-02 22:52:51 +0400

    new: the getUsers module return user info from the database

--- modules/user/getUser.php
+++ modules/user/getUser.php
@@ -1,2 +1,31 @@
 <?php
-// this file get user and return array 
+class getUser {
+    private $session_token;
+    private $pdo;
+
+    function __construct(string $session_token, $pdo) {
+        $this->session_token = $session_token;
+        $this->pdo = $pdo;
+    }   
+
+    function get() : mixed {
+        if (empty($this->session_token)) {
+            return false;
+        }
+
+        try {
+            $stmt = $this->pdo->prepare("SELECT * FROM `users` WHERE `session_token` = ? LIMIT 1");
+            $stmt->execute([$this->session_token]);
+            $user = $stmt->fetch();
+
+            if (empty($user)) {
+                return false;
+            }
+
+            return $user;
+        } catch (PDOException $e) {
+            // logger logic
+            return false;
+        }
+    }   
+}
