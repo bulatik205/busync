0ca6b9b95a731a1b251101f9bb6c6db43373e211
Булат Муллагалиев <bulatmullagal@gmail.com>
2026-02-08 10:26:18 +0400

    new: configs

---
+++ api/v1/config/config.php
@@ -0,0 +1,24 @@
+<?php
+require_once "logger.php";
+require_once "pathGet.php";
+
+# base configuration on MAMP (https://www.mamp.info/)
+$main_host = "localhost"; 
+$main_user = "root"; 
+$main_pass = "root"; 
+$main_db = "busync";
+
+# for local, I use all files in one folder. For production, I will make api.busync.ru (subdomain)
+define('API_PATH', 'api/');
+
+$pdo = new PDO(
+    "mysql:host=$main_host;dbname=$main_db;charset=utf8mb4",
+    $main_user,
+    $main_pass, 
+    [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_EMULATE_PREPARES => true,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]
+);
+?>
---
+++ api/v1/config/pathGet.php
@@ -0,0 +1,28 @@
+<?php
+function getBackPath($fromDir) : string {
+    $root = realpath($_SERVER['DOCUMENT_ROOT']);
+    $from = realpath($fromDir);
+    
+    $root = str_replace('\\', '/', $root);
+    $from = str_replace('\\', '/', $from);
+    
+    if (strpos($from, $root) !== 0) {
+        return '/'; 
+    }
+    
+    $relative = str_replace($root, '', $from);
+    
+    $relative = ltrim($relative, '/');
+    
+    if (empty($relative)) {
+        return './';
+    }
+    
+    $depth = substr_count($relative, '/');
+    
+    if ($depth == 0 && !empty($relative)) {
+        $depth = 1;
+    }
+    
+    return str_repeat('../', $depth);
+}
