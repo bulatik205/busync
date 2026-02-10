a54f9955a3d76ac020afcfa962aecd7849eeeddd
Булат Муллагалиев <bulatmullagal@gmail.com>
2026-02-10 16:05:12 +0400

    new: file and directory api/handlers/post/saveProfile/fieldsValidate.php, which search as JSON input array at valid

---
+++ api/v1/handlers/post/saveProfile/fieldsValidate.php
@@ -0,0 +1,36 @@
+<?php
+class fieldsValidate
+{
+    public array $fields;
+    public array $update;
+
+    public function __construct(array $fields, array $update)
+    {
+        $this->fields = $fields;
+        $this->update = $update;
+    }
+
+    public function validate(): bool
+    {
+        /*
+        * Sorry about O(n^2)
+        */
+        foreach ($this->update as $key) {
+            $isDefined = false;
+
+            foreach ($this->fields as $field) {
+                if ($field !== $key) {
+                    $isDefined = true;
+                    break;
+                }
+            }
+
+            if (!$isDefined) {
+                return false;
+                break;
+            }
+        }
+
+        return true;
+    }
+}
