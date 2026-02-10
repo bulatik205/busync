94bff041d5d30a944018eba34295033243f5394d
Булат Муллагалиев <bulatmullagal@gmail.com>
2026-02-10 22:30:43 +0400

    update: return also fields of users_info

--- api/v1/handlers/get/getMe.php
+++ api/v1/handlers/get/getMe.php
@@ -14,12 +14,23 @@ class getMe {
         try {
             $stmt = $this->pdo->prepare("SELECT id, username, data FROM `users` WHERE `id` = ? LIMIT 1");
             $stmt->execute([$this->userId]);
-            $SQLdata = $stmt->fetch();
+            $SQLUsersData = $stmt->fetch();
+
+            $stmt = $this->pdo->prepare("SELECT first_name, last_name, father_name, business_type, business_site, phone FROM `users_info` WHERE `user_id` = ? LIMIT 1");
+            $stmt->execute([$this->userId]);
+            $SQLUsersInfoData = $stmt->fetch();
 
             $data['success'] = true;
-            $data['user']['id'] = $SQLdata['id'] ?? null;
-            $data['user']['username'] = $SQLdata['username'] ?? null;
-            $data['user']['registration_date'] = $SQLdata['data'] ?? null;
+            $data['user']['id'] = $SQLUsersData['id'] ?? null;
+            $data['user']['username'] = $SQLUsersData['username'] ?? null;
+            $data['user']['registration_date'] = $SQLUsersData['data'] ?? null;
+            $data['user']['first_name'] = $SQLUsersInfoData['first_name'] ?? null;
+            $data['user']['last_name'] = $SQLUsersInfoData['last_name'] ?? null;
+            $data['user']['father_name'] = $SQLUsersInfoData['father_name'] ?? null;
+            $data['user']['business_type'] = $SQLUsersInfoData['business_type'] ?? null;
+            $data['user']['business_site'] = $SQLUsersInfoData['business_site'] ?? null;
+            $data['user']['phone'] = $SQLUsersInfoData['phone'] ?? null;
+            
             return $data;
         } catch (PDOException $e) {
             $data['success'] = false;
