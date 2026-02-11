97402c21f720f5a31fe7a30b0bbc2c6fd6e05f4e
Булат Муллагалиев <bulatmullagal@gmail.com>
2026-02-11 16:09:58 +0400

    update: add ajax and output key string in table

--- sources/js/profile/ajax.js
+++ sources/js/profile/ajax.js
@@ -54,3 +54,61 @@ async function saveProfile(API_KEY) {
         console.log(result.error.message)
     }
 }
+
+async function createApi(API_KEY) {
+    const apiKeysBlock = document.getElementById("api-keys");
+    try {
+        const response = await fetch(`http://localhost/busync/api/v1/createApi/`, {
+            method: 'POST',
+            headers: {
+                'API-key': API_KEY,
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const result = await response.json();
+
+        if (result.success) {
+            const newRow = document.createElement('tr');
+            
+            const apiKey = result.fields.last_inserted_apikey;
+            const apiKeySubstr = result.fields.last_inserted_apikey_substr;
+            
+            newRow.style.backgroundColor = '#5dce8c4e'; 
+            newRow.innerHTML = `
+                <td>${apiKeySubstr}...</td>
+                <td>❌</td>
+                <td></td>
+                <td>${new Date().toLocaleDateString('ru-RU')}</td>
+                <td>
+                    <button onclick="copy('${apiKey}')">
+                        Копировать
+                    </button>
+                </td>
+            `;
+            
+            const createBlock = document.getElementById('create-api-block');
+            apiKeysBlock.insertBefore(newRow, createBlock);
+            
+            successProfieBlock.style.display = "block";
+            setTimeout(() => {
+                successProfieBlock.style.display = "none";
+            }, 2000);
+            
+            newRow.scrollIntoView({ behavior: 'smooth', block: 'center' });
+            
+        } else {
+            errorProfieBlock.style.display = "block";
+            setTimeout(() => {
+                errorProfieBlock.style.display = "none";
+            }, 2000);
+            console.log(result.error.message);
+        }
+    } catch (error) {
+        errorProfieBlock.style.display = "block";
+        setTimeout(() => {
+            errorProfieBlock.style.display = "none";
+        }, 2000);
+        console.log(error.message); 
+    }
+}
