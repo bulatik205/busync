b50c930e9556e66e71d5976467dff68e91f49db6
Булат Муллагалиев <bulatmullagal@gmail.com>
2026-02-12 17:30:42 +0400

    update: added $update variable, which get RAW INPUT and added "json_last_error() !== JSON_ERROR_NONE", which return JSON, which say of the error and return "json_last_error_msg()" - developer know error

--- api/v1/getItems/index.php
+++ api/v1/getItems/index.php
@@ -14,6 +14,8 @@ require_once '../config/config.php';
 define('BASE_PATH', getBackPath(__DIR__));
 
 require_once BASE_PATH . 'api/v1/handlers/validate/validateApiKey.php';
+require_once BASE_PATH . 'api/v1/handlers/get/getItems/getItems.php';
+require_once BASE_PATH . 'api/v1/handlers/get/getItems/validateLimits.php';
 
 $headers = getallheaders();
 
@@ -44,11 +46,21 @@ if (!$validateApiKeyResult['success']) {
     exit;
 }
 
-$userId = (int)$validateApiKeyResult['userId'] ?? null;
+$update = json_decode(file_get_contents('php://input'), true);
 
-if (!is_null($userId) && isset($_SESSION['user_id'])) {
-    $userId = $_SESSION['userId'];
+if (json_last_error() !== JSON_ERROR_NONE) {
+    echo json_encode([
+        'success' => false,
+        'error' => [
+            'code' => 400,
+            'message' => 'Invalid JSON format',
+            'details' => json_last_error_msg()
+        ]
+    ]);
+    exit;
 }
+
+$userId = (int)$validateApiKeyResult['userId'] ?? null;
 
 if (!is_numeric($userId)) {
     echo json_encode([
