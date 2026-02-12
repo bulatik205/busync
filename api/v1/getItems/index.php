bace9951fae810cf7efea94817f2ff92ea5b8a35
Булат Муллагалиев <bulatmullagal@gmail.com>
2026-02-12 07:39:09 +0400

    new: route getItems for get user items

---
+++ api/v1/getItems/index.php
@@ -0,0 +1,63 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+    echo json_encode([
+        'success' => false,
+        'error' => [
+            'code' => 405,
+            'message' => 'Method Not Allowed'
+        ]
+    ]);
+    exit;
+}
+
+require_once '../config/config.php';
+define('BASE_PATH', getBackPath(__DIR__));
+
+require_once BASE_PATH . 'api/v1/handlers/validate/validateApiKey.php';
+
+$headers = getallheaders();
+
+if (!isset($headers['API-key'])) {
+    echo json_encode([
+        'success' => false,
+        'error' => [
+            'code' => 401,
+            'message' => 'Unauthorized'
+        ]
+    ]);
+    exit;
+}
+
+$apiKey = $headers['API-key'];
+
+$validateApiKey = new validateApiKey($apiKey, $pdo);
+$validateApiKeyResult = $validateApiKey->validate();
+
+if (!$validateApiKeyResult['success']) {
+    echo json_encode([
+        'success' => false,
+        'error' => [
+            'code' => $validateApiKeyResult['error']['code'],
+            'message' => $validateApiKeyResult['error']['message']
+        ]
+    ]);
+    exit;
+}
+
+$userId = (int)$validateApiKeyResult['userId'] ?? null;
+
+if (!is_null($userId) && isset($_SESSION['user_id'])) {
+    $userId = $_SESSION['userId'];
+}
+
+if (!is_numeric($userId)) {
+    echo json_encode([
+        'success' => false, 
+        'error' => [
+            'code' => 400, 
+            'message' => 'invalid_user_id'
+        ]
+    ]);
+    exit;
+}
+
