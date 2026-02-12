8d00143543aaf0aa673fc43558591db10b249dfd
Булат Муллагалиев <bulatmullagal@gmail.com>
2026-02-12 17:15:15 +0400

    update: past code validate, if we get undifined fields, script behavior is unpredictable. I added check isset() function

--- api/v1/handlers/get/getItems/validateLimits.php
+++ api/v1/handlers/get/getItems/validateLimits.php
@@ -1,34 +1,47 @@
 <?php
-class validateLimits {
+class validateLimits
+{
     public $limit;
     public $offset;
-    
-    public function __construct($limit, $offset) {
+
+    public function __construct(?int $limit, ?int $offset)
+    {
         $this->limit = $limit;
         $this->offset = $offset;
     }
-    
-    public function validate() : array {
+
+    public function validate(): array
+    {
         $data = [];
-        
-        if (!is_int($this->limit) || !is_int($this->offset)) {
-            $data['success'] = false;
-            $data['error']['message'] = "Invalid type";
-            return $data;
+
+        if (isset($this->limit)) {
+            if (!is_int($this->limit)) {
+                $data['success'] = false;
+                $data['error']['message'] = "Invalid type";
+                return $data;
+            }
+
+            if ($this->limit > 50 || $this->limit < 0) {
+                $data['success'] = false;
+                $data['error']['message'] = "Invalid limit scope";
+                return $data;
+            }
         }
-        
-        if ($this->limit > 50 || $this->limit < 0) {
-            $data['success'] = false;
-            $data['error']['message'] = "Invalid limit scope";
-            return $data;
+
+        if (isset($this->offset)) {
+            if (!is_int($this->offset)) {
+                $data['success'] = false;
+                $data['error']['message'] = "Invalid type";
+                return $data;
+            }
+
+            if ($this->offset > 100000 || $this->offset < 0) {
+                $data['success'] = false;
+                $data['error']['message'] = "Invalid offset scope";
+                return $data;
+            }
         }
-        
-        if ($this->offset > 100000 || $this->offset < 0) {
-            $data['success'] = false;
-            $data['error']['message'] = "Invalid offset scope";
-            return $data;
-        }
-        
+
         $data['success'] = true;
         return $data;
     }
