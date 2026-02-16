54cb5929ec52dec6a20f942fa246870c4c7cf910
Булат Муллагалиев <bulatmullagal@gmail.com>
2026-02-16 23:15:23 +0400

    new: api route for delete item from id

---
+++ api/v1/deleteItem/index.php
@@ -0,0 +1,85 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    echo json_encode([
+        'success' => false,
+        'error' => [
+            'code' => 405,
+            'message' => 'Method Not Allowed'
+        ]
+    ]);
+    exit;
+}
+
+require_once '../config/config.php';
+define('BASE_PATH', getBackPath(__DIR__));
+
+require_once BASE_PATH . 'api/v1/handlers/validate/validateApiKey.php';
+
+$headers = getallheaders();
+
+if (!isset($headers['API-key'])) {
+    echo json_encode([
+        'success' => false,
+        'error' => [
+            'code' => 401,
+            'message' => 'Unauthorized'
+        ]
+    ]);
+    exit;
+}
+
+$apiKey = $headers['API-key'];
+
+$validateApiKey = new validateApiKey($apiKey, $pdo);
+$validateApiKeyResult = $validateApiKey->validate();
+
+if (!$validateApiKeyResult['success']) {
+    echo json_encode([
+        'success' => false,
+        'error' => [
+            'code' => $validateApiKeyResult['error']['code'],
+            'message' => $validateApiKeyResult['error']['message']
+        ]
+    ]);
+    exit;
+}
+
+$userId = (int)$validateApiKeyResult['userId'];
+
+if (!is_numeric($userId)) {
+    echo json_encode([
+        'success' => false,
+        'error' => [
+            'code' => 400,
+            'message' => 'invalid_user_id'
+        ]
+    ]);
+    exit;
+}
+
+$update = json_decode(file_get_contents('php://input'), true);
+
+if (json_last_error() !== JSON_ERROR_NONE) {
+    echo json_encode([
+        'success' => false,
+        'error' => [
+            'code' => 400,
+            'message' => 'Invalid JSON format',
+            'details' => json_last_error_msg()
+        ]
+    ]);
+    exit;
+}
+
+if (!isset($update['fields'])) {
+    echo json_encode([
+        'success' => false,
+        'error' => [
+            'code' => 400,
+            'message' => 'Empty fields'
+        ]
+    ]);
+    exit;
+}
+
+$update['fields']['user_id'] = $userId;
