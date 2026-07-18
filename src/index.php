ee70ce1249f796beff28c129fa0315045ef3bce0
bulatik205 <bulatmullagal@gmail.com>
2026-07-18 17:32:25 +0400

    update: add router and add handler for '/'

--- src/index.php
+++ src/index.php
@@ -1,11 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    Hello world
-</body>
-</html>
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Content-Type');
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') exit;
+
+$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
+    $r->addRoute('GET', '/', 'HomeController@index');
+});
+
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = rawurldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        http_response_code(404);
+        echo json_encode(['error' => 'Not Found']);
+        break;
+        
+    case FastRoute\Dispatcher::FOUND:
+        [$controller, $method] = explode('@', $routeInfo[1]);
+        $params = $routeInfo[2];
+        
+        require_once __DIR__ . "/../src/Controllers/{$controller}.php";
+        
+        $instance = new $controller();
+        $response = $instance->$method($params);
+        echo json_encode($response, JSON_UNESCAPED_UNICODE);
+        break;
+}
